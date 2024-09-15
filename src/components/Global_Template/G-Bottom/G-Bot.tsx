a60bd0c75c6f3636962298590cb4b82454a57ed2
z0opreme <remy.engassac@icloud.com>
2024-09-15 16:15:17 +0200

    x

--- src/components/Global_Template/G-Bottom/G-Bot.tsx
+++ src/components/Global_Template/G-Bottom/G-Bot.tsx
@@ -3,7 +3,7 @@ import Buttons from "./Buttons/Buttons";
 export default function GBot() {
 	return (
 		<div className="bg-cream w-full h-[47.5%] sm:h-[95%] relative top-1 z-[-1] flex items-end">
-			<Buttons/>
+			<Buttons />
 		</div>
 	);
 }
