39adc8a30b0deed0ef88475e36e79a502f1ef112
z0opreme <remy.engassac@icloud.com>
2024-09-17 23:06:46 +0200

    better responsive buttons

--- src/components/Global_Template/G-Bottom/Buttons/Arrow_Btn.tsx
+++ src/components/Global_Template/G-Bottom/Buttons/Arrow_Btn.tsx
@@ -1,7 +1,7 @@
 export default function Arrow_Btn() {
 	return (
 		<div className="w-[30%] h-full flex items-center justify-center">
-			<img src="/assets/SVG/Arrow.svg" alt="Arrow Button" className="w-full max-w-[350px] ml-12 md:ml-24 mb-24" />
+			<img src="/assets/SVG/Arrow.svg" alt="Arrow Button" className="w-[85%] min-w-[110px] sm:w-full md:min-w-[190px] max-w-[350px] ml-12 md:ml-24 mb-24" />
 		</div>
 	);
 }
