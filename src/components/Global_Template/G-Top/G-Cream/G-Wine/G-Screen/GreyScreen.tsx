fd96ef8d7e9d50594a939ef9b5bc67cd699e04ed
z0opreme <remy.engassac@icloud.com>
2024-09-17 23:12:28 +0200

    delete greyscreeninfo, intermediate component useless

--- src/components/Global_Template/G-Top/G-Cream/G-Wine/G-Screen/GreyScreen.tsx
+++ src/components/Global_Template/G-Top/G-Cream/G-Wine/G-Screen/GreyScreen.tsx
@@ -1,11 +1,12 @@
-import GreyScreenInfo from "./GreyScreenInfo";
+import TopScreen from "./TopScreen";
+import BottomScreen from "./BottomScreen";
 
 export default function GreyScreen({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="bg-greyScreen h-[89%] w-[94%] sm:h-[91%] lg:h-[90%] sm:w-[95%] rounded-xl md:rounded-2x shadow-[inset_0_0_0.5rem_0.18rem_#363636]">
-			<GreyScreenInfo>
-				{children}
-			</GreyScreenInfo>
+		<div className="bg-greyScreen h-[89%] w-[94%] sm:h-[91%] lg:h-[90%] sm:w-[95%] flex flex-col rounded-xl md:rounded-2x shadow-[inset_0_0_0.5rem_0.18rem_#363636]">
+			<TopScreen />
+			{children}
+			<BottomScreen />
 		</div>
 	);
 }
