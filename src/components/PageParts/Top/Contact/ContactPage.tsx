75af9ac309770b0508f1c74e5fe3d55c23d8db09
z0opreme <remy.engassac@icloud.com>
2024-09-22 15:10:53 +0200

    add react hook form and finish responsive form contact

--- src/components/PageParts/Top/Contact/ContactPage.tsx
+++ src/components/PageParts/Top/Contact/ContactPage.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import BackgroundSign from "@/components/Global_Template/G-Top/G-Cream/G-Wine/G-Screen/BackgroundSign";
+import { useState, useEffect } from "react";
 
 export default function ContactPage() {
+	const [lastName, setLastName] = useState("");
+	const [firstName, setFirstName] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
+	const [isButtonVisible, setIsButtonVisible] = useState(false);
+
+	useEffect(() => {
+		setIsButtonVisible(lastName && firstName && email && message ? true : false);
+	}, [lastName, firstName, email, message]);
+
 	return (
 		<div className="flex flex-row w-full h-full">
 			<nav className="flex flex-col justify-center items-center w-1/3 h-full relative z-1">
@@ -28,31 +41,35 @@ export default function ContactPage() {
 			</nav>
 
 			<div className="flex flex-col justify-center items-center w-1/3 h-full">
-				<h1 className="font-pressStart2P text-pretty text-center absolute w-[85%] top-10 md:top-16 lg:top-18 xl:top-18 text-xs sm:text-base md:text-2xl lg:text-3xl">Interréssé ? Curieux ? N'attendez plus, Contactez moi !</h1>
+				<h1 className="font-pressStart2P text-pretty text-center absolute w-[85%] top-10 md:top-16 lg:top-18 xl:top-18 text-xs sm:text-sm md:text-2xl">Interréssé ? Curieux ? N'attendez plus, Contactez moi !</h1>
 				<BackgroundSign />
 			</div>
 
-			<div className="flex flex-col justify-center items-center w-1/3 h-full gap-1">
-				<form className="flex flex-col gap-4">
-					<div className="flex flex-col gap-1">
-						<label htmlFor="lastName" className="font-pressStart2P text-[0.7rem] sm:text-sm md:text-base lg:text-lg">Nom :</label>
-						<input type="text" id="lastName" name="lastName" />
+			<div className="flex flex-col justify-end items-start w-1/3 h-full relative z-1">
+				<form className="w-[90%] h-full justify-end flex flex-col gap-2 sm:gap-1 md:gap-4">
+					<div className="w-full flex flex-col">
+						<label htmlFor="lastName" className="font-pressStart2P text-[0.7rem] sm:text-xs md:text-sm lg:text-base">Nom :</label>
+						<input type="text" id="lastName" name="lastName" onChange={(e) => setLastName(e.target.value)} className="w-full bg-gray-300 border-[0.09rem] px-1 md:py-1 font-cabin border-gray-400 focus:border-gray-800 focus:outline-none" />
 					</div>
 
-					<div className="flex flex-col gap-1">
-						<label htmlFor="firstName" className="font-pressStart2P text-[0.7rem] sm:text-sm md:text-base lg:text-lg">Prénom :</label>
-						<input type="text" id="firstName" name="firstName" className="bg-gray-300 border-1" />
+					<div className="w-full flex flex-col">
+						<label htmlFor="firstName" className="font-pressStart2P text-[0.7rem] sm:text-xs md:text-sm lg:text-base">Prénom :</label>
+						<input type="text" id="firstName" name="firstName" onChange={(e) => setFirstName(e.target.value)} className="w-full bg-gray-300 border-[0.09rem] px-1 md:py-1 font-cabin border-gray-400 focus:border-gray-800 focus:outline-none" />
 					</div>
 
-
-					<div className="flex flex-col gap-1">
-						<label htmlFor="email" className="font-pressStart2P text-[0.7rem] sm:text-sm md:text-base lg:text-lg">Mail :</label>
-						<input type="email" id="email" name="email" />
+					<div className="w-full flex flex-col">
+						<label htmlFor="email" className="font-pressStart2P text-[0.7rem] sm:text-xs md:text-sm lg:text-base">Mail :</label>
+						<input type="email" id="email" name="email" onChange={(e) => setEmail(e.target.value)} className="w-full bg-gray-300 border-[0.09rem] px-1 md:py-1 font-cabin border-gray-400 focus:border-gray-800 focus:outline-none" />
 					</div>
 
-					<div className="flex flex-col gap-1">
-						<label htmlFor="message" className="font-pressStart2P text-[0.7rem] sm:text-sm md:text-base lg:text-lg">Message :</label>
-						<textarea id="message" name="message" />
+					<div className="w-full flex flex-col">
+						<label htmlFor="message" className="font-pressStart2P text-[0.7rem] sm:text-xs md:text-sm lg:text-base">Message :</label>
+						<textarea id="message" name="message" onChange={(e) => setMessage(e.target.value)} className="w-full md:h-40 bg-gray-300 border-[0.09rem] px-1 md:py-1 font-cabin border-gray-400 focus:border-gray-800 focus:outline-none resize-none" />
+					</div>
+					<div className="flex justify-center items-center w-full">
+						{isButtonVisible && (
+							<button type="submit" className="w-1/2 h-auto p-1 rounded bg-wine hover:bg-wine/80 text-cream text-center text-wrap text-[0.5rem] sm:text-[0.7rem] md:text-xs lg:text-base font-pressStart2P">Envoyer</button>
+						)}
 					</div>
 				</form>
 			</div>
