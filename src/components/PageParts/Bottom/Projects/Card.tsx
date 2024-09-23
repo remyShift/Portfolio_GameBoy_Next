69e1a2a3666e0eb47386a3000ab1912a96c1c659
z0opreme <remy.engassac@icloud.com>
2024-09-23 10:07:49 +0200

    add link to projects

--- src/components/PageParts/Bottom/Projects/Card.tsx
+++ src/components/PageParts/Bottom/Projects/Card.tsx
@@ -4,7 +4,7 @@ export default function Card({ project }: { project: any }) {
 	project.stacks[0] === "lua" ? classStacks = "w-[40%] object-contain" : classStacks = "w-[15%] object-contain"
 
 	return (
-		<a href="#" className="flex flex-col items-center bg-wine border-[#C6AF87] hover:bg-wine/80 border-solid border-4 rounded-lg shadow md:flex-row md:max-w-xl ">
+		<a href={project.link} className="flex flex-col items-center bg-wine border-[#C6AF87] hover:bg-wine/80 border-solid border-4 rounded-lg shadow md:flex-row md:max-w-xl ">
 			<img className="object-cover w-full rounded-t-lg md:h-auto md:w-[40%] md:rounded-none md:rounded-s-lg" src={project.image} alt={project.name} />
 			<div className="flex flex-col h-full justify-between p-4 leading-normal">
 				<h5 className="mb-2 text-xl font-cabinBold tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
