1b8927ca7052be7dd4427a2b011704eff6f1634b
z0opreme <remy.engassac@icloud.com>
2024-09-23 22:06:27 +0200

    better breadcrumb

--- src/components/Global_Template/G-Top/G-Cream/G-Wine/G-Screen/TopScreen.tsx
+++ src/components/Global_Template/G-Top/G-Cream/G-Wine/G-Screen/TopScreen.tsx
@@ -15,7 +15,7 @@ export default function TopScreen() {
 		return parts.map((part, index) => {
 			const linkPath = part === "@ACCUEIL" ? '/' : '/' + parts.slice(1, index + 1).join('/').toLowerCase();
 			return {
-				label: part === "@ACCUEIL" ? "@ACCUEIL" : validPaths.includes('/' + part.toLowerCase()) ? `/${part}` : `/404`,
+				label: part === "@ACCUEIL" ? "@ACCUEIL" : validPaths.includes('/' + part.toLowerCase()) ? `${part}` : `404`,
 				path: linkPath
 			};
 		});
@@ -33,12 +33,15 @@ export default function TopScreen() {
 				{links.map((link, index) => (
 					<Link key={index} href={link.path} className="text-greyTextInfo font-cabinBold text-[0.6rem] sm:text-xs md:text-base lg:text-xl hover:text-wine hover:underline">
 						{link.label}
+						{(index < links.length - 1) && (
+							<span className="text-greyTextInfo mx-0.5 font-cabinBold text-[0.6rem] sm:text-xs md:text-base lg:text-xl"> &gt; </span>
+						)}
 					</Link>
 				))}
 			</div>
 			<h1 className="text-greyTextInfo font-cabinBold text-[0.6rem] sm:text-xs md:text-base lg:text-xl pr-2.5">
 				{generateBatteryStatus()}
 			</h1>
-		</header>
+		</header >
 	);
 }
