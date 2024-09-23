e4159b5b06760d9530f8446c07fd34711b8e5d9b
z0opreme <remy.engassac@icloud.com>
2024-09-23 21:44:33 +0200

    fix some issue

--- src/components/PageParts/Bottom/Projects/Card.tsx
+++ src/components/PageParts/Bottom/Projects/Card.tsx
@@ -1,18 +1,18 @@
 export default function Card({ project, index }: { project: any, index: number }) {
 	let classStacks: string;
 	console.log(project.stacks)
-	project.stacks[0] === "lua" ? classStacks = "w-[15%] md:w-[30%] max-w-[50px] md:max-w-none object-contain" : classStacks = "w-[12%] max-w-[50px] md:max-w-none object-contain"
+	project.stacks[0] === "lua" ? classStacks = "w-[8%] md:w-[30%] max-w-[50px] lg:min-w-[40px] md:max-w-none object-contain" : classStacks = "w-[7%] max-w-[50px] lg:min-w-[40px] md:max-w-none object-contain"
 
 	const isEven = index % 2 === 0;
 
 	return (
 		<a href={project.link} className="flex flex-col justify-between items-center bg-wine border-[#C6AF87] hover:bg-wine/80 border-solid border-4 rounded-xl shadow md:flex-row md:w-[90%] md:h-[7%]">
-			{isEven ? (
+			{isEven && (
 				<img className="object-contain w-full rounded-t-lg md:h-full md:w-[40%] md:rounded-none md:rounded-l-lg" src={project.image} alt={project.name} />
-			) : null}
+			)}
 			<div className={`flex flex-col h-full justify-between p-4 leading-normal ${isEven ? 'md:items-end' : 'md:items-start'}`}>
-				<h5 className="mb-2 text-xl font-cabinBold tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
-				<p className="mb-3 font-cabin text-gray-400">{project.description}</p>
+				<h5 className="mb-2 text-xl md:text-2xl font-cabinBold text-white">{project.name}</h5>
+				<p className="font-cabin text-sm lg:text-base text-white">{project.description}</p>
 				<div className={`flex flex-wrap ${isEven ? 'md:justify-end' : 'md:justify-start'} gap-2 my-2`}>
 					{project.stacks.map((stack: string) => (
 						<img key={stack} className={classStacks} src={`/assets/img/stacks/${stack}.png`} alt={stack} />
@@ -20,9 +20,9 @@ export default function Card({ project, index }: { project: any, index: number }
 				</div>
 			</div>
 			{
-				!isEven ? (
+				!isEven && (
 					<img className="object-cover w-full rounded-t-lg md:h-full md:w-[40%] md:rounded-none md:rounded-r-lg" src={project.image} alt={project.name} />
-				) : null
+				)
 			}
 		</a >
 	);
--- src/components/PageParts/Bottom/Projects/Divider.tsx
+++ src/components/PageParts/Bottom/Projects/Divider.tsx
@@ -1,11 +1,13 @@
 export default function Divider({ children, onClick }: { children: React.ReactNode, onClick?: () => void }) {
 	return (
-		<div
-			className={`flex w-[80%] items-center rounded-full my-4 md:my-6 lg:my-10 ${onClick ? 'hover:underline cursor-pointer' : ''}`}
-			onClick={onClick}
-		>
+		<div className="flex w-[80%] items-center rounded-full my-4 md:my-6 lg:my-10">
 			<div className="flex-1 border-b border-wine"></div>
-			<h1 className="text-black font-cabinBold text-base md:text-xl lg:text-2xl px-3">{children}</h1>
+			<h1
+				className={`text-black font-cabinBold text-xl md:text-2xl lg:text-3xl px-3 ${onClick ? 'rounded-full hover:bg-wine hover:text-white cursor-pointer border-2 border-wine' : ''}`}
+				onClick={onClick}
+			>
+				{children}
+			</h1>
 			<div className="flex-1 border-b border-wine"></div>
 		</div>
 	);
