db3c7e6f183fdcb7c6524fca8f6cc0d196c41d8d
z0opreme <remy.engassac@icloud.com>
2024-09-24 01:01:25 +0200

    x

--- src/components/PageParts/Bottom/About/TimelineCard.tsx
+++ src/components/PageParts/Bottom/About/TimelineCard.tsx
@@ -9,12 +9,12 @@ export default function TimelineContent({ title, date, description }: TimelineCo
 		<div className="ml-6 flex flex-col w-full md:max-w-lg border-[#C6AF87] border-2 rounded-lg bg-wine p-4 md:p-6 shadow-lg shadow-black/30">
 			<div className="mb-4 flex justify-between gap-4">
 				<p
-					className="text-md lg:text-xl font-openSansBold text-white"
+					className="text-sm md:text-lg lg:text-xl font-openSansBold text-white"
 				>
 					{title}
 				</p>
 				<p
-					className="text-sm lg:text-base font-openSansItalic text-white"
+					className="text-xs sm:text-sm lg:text-base font-openSansItalic text-white"
 				>
 					{date}
 				</p>
