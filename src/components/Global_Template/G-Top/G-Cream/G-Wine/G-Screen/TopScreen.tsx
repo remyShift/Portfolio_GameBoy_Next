6d490e91e7d5647910902779213385f569045b0d
z0opreme <remy.engassac@icloud.com>
2024-09-26 10:08:47 +0200

    few edits

--- src/components/Global_Template/G-Top/G-Cream/G-Wine/G-Screen/TopScreen.tsx
+++ src/components/Global_Template/G-Top/G-Cream/G-Wine/G-Screen/TopScreen.tsx
@@ -3,26 +3,25 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { useValidPaths } from "@/context/store";
-import { div } from "framer-motion/client";
 
 export default function TopScreen() {
 	const { validPaths } = useValidPaths();
 	const pathname = usePathname();
 
-	const generateLinks = (path: string) => {
+	const generateLinksProps = (path: string) => {
 		const basePath = path === "/" ? "@ACCUEIL" : `@ACCUEIL/${path.substring(1).toUpperCase().trim()}`;
 		const parts = basePath.split('/');
 
 		return parts.map((part, index) => {
-			const linkPath = part === "@ACCUEIL" ? '/' : '/' + parts.slice(1, index + 1).join('/').toLowerCase();
-			return {
-				label: part === "@ACCUEIL" ? "@ACCUEIL" : validPaths.includes('/' + part.toLowerCase()) ? `${part}` : `404`,
-				path: linkPath
-			};
+			const isWelcomePage = part === "@ACCUEIL";
+			const linkPath = isWelcomePage ? '/' : '/' + parts.slice(1, index + 1).join('/').toLowerCase();
+			const label = isWelcomePage ? "@ACCUEIL" : validPaths.includes('/' + part.toLowerCase()) ? part : "404";
+
+			return { label, path: linkPath };
 		});
 	};
 
-	const links = generateLinks(pathname);
+	const linksProps = generateLinksProps(pathname);
 
 	const generateBatteryStatus = () => {
 		return validPaths.includes(pathname) ? "BATTERIE PLEINE" : "BATTERIE VIDE";
@@ -31,12 +30,12 @@ export default function TopScreen() {
 	return (
 		<header className="flex justify-between mt-1 sm:mt-2 relative z-1">
 			<div className="flex pl-2">
-				{links.map((link, index) => (
+				{linksProps.map((link, index) => (
 					<div key={index} className="flex items-center gap-0.5 md:gap-2">
 						<Link href={link.path} className="text-greyTextInfo font-openSansBold text-[0.6rem] sm:text-xs md:text-base lg:text-xl hover:text-wine hover:underline">
 							{link.label}
 						</Link>
-						{(index < links.length - 1) && (
+						{(index < linksProps.length - 1) && (
 							<span className="text-greyTextInfo font-openSansBold text-[0.6rem] sm:text-xs md:text-base lg:text-xl"> &gt; </span>
 						)}
 					</div>
