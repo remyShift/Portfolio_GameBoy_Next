57a22b6aa111586a57755f532f1f1aa7e7e7f2b8
z0opreme <remy.engassac@icloud.com>
2024-09-26 10:13:35 +0200

    code cleaned

--- src/components/Global_Template/G-Top/G-Cream/G-Wine/G-Screen/TopScreen.tsx
+++ src/components/Global_Template/G-Top/G-Cream/G-Wine/G-Screen/TopScreen.tsx
@@ -8,7 +8,7 @@ export default function TopScreen() {
 	const { validPaths } = useValidPaths();
 	const pathname = usePathname();
 
-	const generateLinksProps = (path: string) => {
+	const generateBreadcrumbLinks = (path: string) => {
 		const basePath = path === "/" ? "@ACCUEIL" : `@ACCUEIL/${path.substring(1).toUpperCase().trim()}`;
 		const parts = basePath.split('/');
 
@@ -17,32 +17,32 @@ export default function TopScreen() {
 			const linkPath = isWelcomePage ? '/' : '/' + parts.slice(1, index + 1).join('/').toLowerCase();
 			const label = isWelcomePage ? "@ACCUEIL" : validPaths.includes('/' + part.toLowerCase()) ? part : "404";
 
-			return { label, path: linkPath };
+			return (
+				<Link href={linkPath} className="text-greyTextInfo font-openSansBold text-[0.6rem] sm:text-xs md:text-base lg:text-xl hover:text-wine hover:underline">
+					{label}
+				</Link>
+			)
 		});
 	};
 
-	const linksProps = generateLinksProps(pathname);
+	const breadcrumbLinks = generateBreadcrumbLinks(pathname);
 
-	const generateBatteryStatus = () => {
-		return validPaths.includes(pathname) ? "BATTERIE PLEINE" : "BATTERIE VIDE";
-	}
+	const batteryStatus = validPaths.includes(pathname) ? "BATTERIE PLEINE" : "BATTERIE VIDE";
 
 	return (
 		<header className="flex justify-between mt-1 sm:mt-2 relative z-1">
 			<div className="flex pl-2">
-				{linksProps.map((link, index) => (
+				{breadcrumbLinks.map((link, index) => (
 					<div key={index} className="flex items-center gap-0.5 md:gap-2">
-						<Link href={link.path} className="text-greyTextInfo font-openSansBold text-[0.6rem] sm:text-xs md:text-base lg:text-xl hover:text-wine hover:underline">
-							{link.label}
-						</Link>
-						{(index < linksProps.length - 1) && (
-							<span className="text-greyTextInfo font-openSansBold text-[0.6rem] sm:text-xs md:text-base lg:text-xl"> &gt; </span>
+						{link}
+						{(index < breadcrumbLinks.length - 1) && (
+							<span className="text-greyTextInfo font-openSansBold text-[0.6rem] mr-1 sm:text-xs md:text-base lg:text-xl"> &gt; </span>
 						)}
 					</div>
 				))}
 			</div>
 			<h1 className="text-greyTextInfo font-openSansBold text-[0.6rem] sm:text-xs md:text-base lg:text-xl pr-2.5">
-				{generateBatteryStatus()}
+				{batteryStatus}
 			</h1>
 		</header >
 	);
