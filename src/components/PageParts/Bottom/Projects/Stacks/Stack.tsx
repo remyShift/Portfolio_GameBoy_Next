547ae9823006373f2b37005c2f68acc0b357fd71
z0opreme <remy.engassac@icloud.com>
2024-09-27 22:09:28 +0200

    some refacto2

--- src/components/PageParts/Bottom/Projects/Stacks/Stack.tsx
+++ src/components/PageParts/Bottom/Projects/Stacks/Stack.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function Stack({ stack }: { stack: string }) {
 	return (
 		<>
-			<Image src={`/assets/img/stacks/${stack}.webp`} alt={stack} width={60} height={60} />
+			<Image
+				className="w-6 h-6 sm:w-10 sm:h-10 md:w-14 md:h-14 lg:w-20 lg:h-20"
+				src={`/assets/img/stacks/${stack}.webp`}
+				alt={stack}
+				width={60}
+				height={60}
+			/>
 		</>
 	);
 }
