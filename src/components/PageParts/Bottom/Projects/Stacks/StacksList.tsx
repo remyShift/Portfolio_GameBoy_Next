91b6c0308bca63c142f04e060abaea6dae2b6801
Remy Cassagne <remy.engassac@icloud.com>
2024-11-25 22:30:34 +0100

    added hairbnb project

--- src/components/PageParts/Bottom/Projects/Stacks/StacksList.tsx
+++ src/components/PageParts/Bottom/Projects/Stacks/StacksList.tsx
@@ -10,10 +10,13 @@ export default function StacksList() {
 		"lua",
 		"next",
 		"react",
+		"ruby",
+		"rails",
+		"sql"
 	]
 
 	return (
-		<div className="w-full h-[10%] sm:h-[15%] md:h-[25%] lg:h-[35%] flex justify-center items-center gap-5 md:gap-7 my-4">
+		<div className="w-full flex flex-wrap justify-center items-center gap-5 md:gap-7 my-4">
 			{stacks.map((stack) => (
 				<Stack key={stack} stack={stack} />
 			))}
