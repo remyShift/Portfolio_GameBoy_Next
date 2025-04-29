d869a74162a4328405ebf1d69feeee8d13388950
remyShift <remy.engassac@icloud.com>
2025-04-29 23:14:26 +0200

    fix projects list overflow issue

--- src/components/PageParts/Bottom/Projects/ProjectsList.tsx
+++ src/components/PageParts/Bottom/Projects/ProjectsList.tsx
@@ -4,7 +4,7 @@ import projectsData from "../../../../utils/projects.json";
 export default function ProjectsList() {
 	const projects = projectsData.projects;
 	return (
-		<div className="flex flex-wrap justify-center gap-4">
+		<div className="flex flex-wrap justify-center gap-4 mb-28">
 			{projects.map((project, index) => (
 				<Card key={index} project={project} index={index} />
 			))}
