dd4f3642a162a6499c6de98cdc88023a80868188
remyShift <remy.engassac@icloud.com>
2025-09-07 13:38:52 +0200

    feat: update kicksfolio link and display of contact form

--- src/components/PageParts/Top/Contact/ContactPage.tsx
+++ src/components/PageParts/Top/Contact/ContactPage.tsx
@@ -62,7 +62,7 @@ export default function ContactPage() {
 				<BackgroundSign />
 			</div>
 
-			<div className="flex flex-col justify-center w-1/3 h-full relative z-1">
+			<div className="hidden md:flex flex-col justify-center w-1/3 h-full relative z-1">
 				<form onSubmit={onSubmitHandler} className="w-[95%] sm:w-[90%] h-full flex flex-col justify-end gap-1 md:gap-4">
 					<div className="w-full flex flex-col justify-center">
 						<label htmlFor="lastName" className="font-pressStart2P text-[0.6rem] sm:text-xs md:text-sm lg:text-base">Nom :</label>
