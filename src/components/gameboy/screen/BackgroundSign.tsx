39dad1e1605999f633699db2552721f1ba7845eb
remyShift <remy.engassac@icloud.com>
2026-04-23 23:39:54 +0200

    fix: update BackgroundSign component layout for improved flexibility

--- src/components/gameboy/screen/BackgroundSign.tsx
+++ src/components/gameboy/screen/BackgroundSign.tsx
@@ -1,6 +1,6 @@
 export default function BackgroundSign() {
 	return (
-		<div className="w-full h-full flex items-center justify-center">
+		<div className="flex-1 h-full flex items-center justify-center">
 			<span aria-hidden="true" className="font-gillSans font-bold text-7xl sm:text-8xl lg:text-[20dvh] xl:text-[25dvh] bg-clip-text text-transparent bg-black/25"
 				style={{ textShadow: "0 0.2rem 0.4rem #CFCCCC" }}>
 				&lt;/&gt;
